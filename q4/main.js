86f12a7603088b2031e4fcd79d77d1f9b2633807
dolphinlin <hisb50918@gmail.com>
2017-05-06 09:51:44 +0800

    add_q4_function

--- q4/main.js
+++ q4/main.js
@@ -1,9 +1,29 @@
+const filters = {
+  all(todos) {
+    return todos
+  },
+  completed(todos) {
+    return todos.filter(todo => todo.complete)
+  },
+  uncomplete(todos) {
+    return todos.filter(todo => !todo.complete)
+  }
+}
+
 new Vue({
   el: '#app',
   data: {
     todos: [],
     newTodo: '',
-    todoindex: 0
+    todoindex: 0,
+    filterBy: 'all',
+    editable: false,
+    editObject: {}
+  },
+  computed: {
+    todosFilter() {
+      return filters[this.filterBy](this.todos)
+    }
   },
   methods: {
     addItem() {
@@ -16,6 +36,24 @@ new Vue({
     },
     removeTodo(i) {
       this.todos.splice(this.todos.indexOf(i), 1)
+    },
+    changeFilter(f) {
+      this.filterBy = f
+    },
+    clear() {
+      this.todos.forEach((todo) => {
+        if (todo.complete) {
+          this.removeTodo(todo)
+        }
+      })
+    },
+    editTodo(todo) {
+      this.editObject = todo
+      this.editable = true
+    },
+    completeEdit() {
+      this.editObject = {}
+      this.editable = false
     }
   }
 })
